7de49fb4f4aa701c169e456eb62e002062af1f62
dmaack <dominiquemaack@gmail.com>
2020-07-09 14:54:17 -0600

    connected products to polaris button component

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b566119306cebc870d40";
+/******/ 	var hotCurrentHash = "eee09ef110ed1fd075f7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
